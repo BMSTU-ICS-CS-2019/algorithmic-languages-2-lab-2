49c893acb3d3c5ed3c214c443aeb3535c18fe606
PROgrm_JARvis <mrjarviscraft@gmail.com>
2020-03-06 14:31:54 +0300

    Add Region class

---
+++ region.h
@@ -0,0 +1,28 @@
+#ifndef ALGORITHMIC_LANGUAGES_2_LAB_2_REGION_H
+#define ALGORITHMIC_LANGUAGES_2_LAB_2_REGION_H
+
+
+#include <string>
+
+class Region {
+protected:
+    const std::string name_;
+    long long per_family_payout_;
+    long double payout_threshold_;
+public:
+
+    Region(std::string name, long long per_family_payout, long double payout_threshold);
+
+    [[nodiscard]] std::string name() const;
+
+    [[nodiscard]] long long per_family_payout() const;
+
+    void per_family_payout(long long per_family_payout);
+
+    [[nodiscard]] long double payout_threshold() const;
+
+    void payout_threshold(long double payout_threshold);
+};
+
+
+#endif //ALGORITHMIC_LANGUAGES_2_LAB_2_REGION_H
