3bb566465cc3b5983976fb88287ee16df5b67bda
PROgrm_JARvis <mrjarviscraft@gmail.com>
2020-03-06 17:33:12 +0300

    Add simple usage example to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "regional_family.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    lab::Region region("MSK", 100, 0.5);
+    lab::RegionalFamily family_1(
+            "Greens", 4u, 100000, std::make_shared<lab::Region>(region)
+    );
+    lab::RegionalFamily family_2(
+            "Other", 4u, 100000, std::make_shared<lab::Region>(region)
+    );
+
+    std::cout << "Family 1: " << family_1 << std::endl;
+    std::cout << "Family 2: " << family_2 << std::endl;
+
     return 0;
 }
