89a34ccf0237819dfc3ca848c2f47cf75669e4e8
PROgrm_JARvis <mrjarviscraft@gmail.com>
2020-03-06 14:35:23 +0300

    Add RegionalFamily class

---
+++ regional_family.cpp
@@ -0,0 +1,1 @@
+#include "regional_family.h"
---
+++ regional_family.h
@@ -0,0 +1,15 @@
+#ifndef ALGORITHMIC_LANGUAGES_2_LAB_2_REGIONAL_FAMILY_H
+#define ALGORITHMIC_LANGUAGES_2_LAB_2_REGIONAL_FAMILY_H
+
+
+#include "family.h"
+
+namespace lab {
+
+    class RegionalFamily : public Family {
+
+    };
+}
+
+
+#endif //ALGORITHMIC_LANGUAGES_2_LAB_2_REGIONAL_FAMILY_H
