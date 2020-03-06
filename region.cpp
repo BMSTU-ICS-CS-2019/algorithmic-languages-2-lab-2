fc33199a2cea9befa49a65165ce45bc2721b7ea6
PROgrm_JARvis <mrjarviscraft@gmail.com>
2020-03-06 14:35:01 +0300

    Move region to the corresponding names[ace

--- region.cpp
+++ region.cpp
@@ -1,24 +1,26 @@
 #include "region.h"
 
-Region::Region(std::string name, long long per_family_payout, long double payout_threshold)
-        : name_(std::move(name)), per_family_payout_(per_family_payout), payout_threshold_(payout_threshold) {}
+namespace lab {
+    Region::Region(std::string name, long long per_family_payout, long double payout_threshold)
+            : name_(std::move(name)), per_family_payout_(per_family_payout), payout_threshold_(payout_threshold) {}
 
-std::string Region::name() const {
-    return name_;
-}
+    std::string Region::name() const {
+        return name_;
+    }
 
-long long Region::per_family_payout() const {
-    return per_family_payout_;
-}
+    long long Region::per_family_payout() const {
+        return per_family_payout_;
+    }
 
-void Region::per_family_payout(long long per_family_payout) {
-    per_family_payout_ = per_family_payout;
-}
+    void Region::per_family_payout(long long per_family_payout) {
+        per_family_payout_ = per_family_payout;
+    }
 
-long double Region::payout_threshold() const {
-    return payout_threshold_;
-}
+    long double Region::payout_threshold() const {
+        return payout_threshold_;
+    }
 
-void Region::payout_threshold(long double payout_threshold) {
-    payout_threshold_ = payout_threshold;
+    void Region::payout_threshold(long double payout_threshold) {
+        payout_threshold_ = payout_threshold;
+    }
 }
--- region.h
+++ region.h
@@ -4,25 +4,27 @@
 
 #include <string>
 
-class Region {
-protected:
-    const std::string name_;
-    long long per_family_payout_;
-    long double payout_threshold_;
-public:
+namespace lab {
+    class Region {
+    protected:
+        const std::string name_;
+        long long per_family_payout_;
+        long double payout_threshold_;
+    public:
 
-    Region(std::string name, long long per_family_payout, long double payout_threshold);
+        Region(std::string name, long long per_family_payout, long double payout_threshold);
 
-    [[nodiscard]] std::string name() const;
+        [[nodiscard]] std::string name() const;
 
-    [[nodiscard]] long long per_family_payout() const;
+        [[nodiscard]] long long per_family_payout() const;
 
-    void per_family_payout(long long per_family_payout);
+        void per_family_payout(long long per_family_payout);
 
-    [[nodiscard]] long double payout_threshold() const;
+        [[nodiscard]] long double payout_threshold() const;
 
-    void payout_threshold(long double payout_threshold);
-};
+        void payout_threshold(long double payout_threshold);
+    };
+}
 
 
 #endif //ALGORITHMIC_LANGUAGES_2_LAB_2_REGION_H
